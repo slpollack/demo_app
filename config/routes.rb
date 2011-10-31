120dda1b0e5843a33e5c4d3e43cc4239c9c4f59c
Scott Pollack <scott@slpollack.com>
2011-10-30 22:14:17 -0400

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 DemoApp::Application.routes.draw do
-  get "pages/home"
-
-  get "pages/contact"
- 
-  get "pages/about"
-
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
+  root :to => 'pages#home'  
+   
   resources :microposts
 
   resources :users
