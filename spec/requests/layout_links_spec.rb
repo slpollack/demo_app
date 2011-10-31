79edc14287265b331d0f195dc34ff65a683cbe55
Scott Pollack <scott@slpollack.com>
2011-10-30 22:35:36 -0400

    Finished layout and routes

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -2,13 +2,6 @@ require 'spec_helper'
 
 describe "LayoutLinks" do
   
-  describe "GET /layout_links" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get layout_links_index_path
-      response.status.should be(200)
-    end
-  end
 
   it "should have a Home page at '/'" do
 	get '/'
@@ -28,6 +21,11 @@ describe "LayoutLinks" do
   it "should have a Help page at '/'" do
 	get '/help'
 	response.should have_selector('title', :content => "Help")
+  end
+  
+  it "should have a Signup page at '/signup'" do
+	get '/signup'
+	response.should have_selector('title', :content => "Sign up")
   end
   
   
