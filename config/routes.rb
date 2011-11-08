870570825b1c4d2383bbbf18c9af9a9ac1122d44
Scott Pollack <scott@slpollack.com>
2011-11-07 22:25:49 -0500

    Finished chapter 9.1.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 DemoApp::Application.routes.draw do
+  get "sessions/new"
+
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
+
+
 
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
   match '/signup', :to => 'users#new'
+  match '/signin', :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
+
   root :to => 'pages#home'  
    
    
